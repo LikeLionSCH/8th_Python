d5d066a69d1586be6b5dbe0a49946c2065b51b37
ningpop <craft1933@naver.com>
2020-06-16 01:11:51 +0900

    first commit

---
+++ Python/Basic 1 [06.01]/0601_hw1.py
@@ -0,0 +1,8 @@
+num = int(input("숫자를 입력하세요: "))
+
+if num > 0:
+    print(num, "은 양수입니다.")
+elif num < 0:
+    print(num, "은 음수 입니다.")
+else:
+    print(num, "은 양수도 음수도 아닙니다.")
---
+++ Python/Basic 1 [06.01]/0601_hw2.py
@@ -0,0 +1,8 @@
+result = 0
+i = 1
+
+while i <= 100:
+    result = result + i
+    i = i + 1
+
+print("1부터 100까지의 합은", result, "입니다.")
